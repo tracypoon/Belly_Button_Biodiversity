9dd9d2bf8b1bc062e3a8b19b9c18c383f99a1e43
Tracy Poon <tracypoon88@gmail.com>
2022-03-23 22:37:35 -0700

    Second Homework Upload

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,95 +1,101 @@
-// Fetch the JSON data and console log it
-// d3.json("samples.json").then(function(data1) {
-//     console.log(data1);
-//   });
-
-//Call updatePlotly() when a change takes place to the DOM
-// d3.selectAll("#selDataset").on("change", updatePlotly);
-
 //Create dropdown menu
-function optionChanged(sampleid){
-  console.log(sampleid)
+function optionChanged(sample_id) {
+  demo_table(sample_id);
+  console.log(sample_id);
+  chart(sample_id);
 }
+
 function dropdown() {
   var dropdownMenu = d3.select("#selDataset");
-  d3.json("samples.json").then(function(data) {
-      var id_names = data.names;
-      id_names.forEach((id) => {
-          dropdownMenu.append("option").text(id).property("value",id)
-      });
-      //bar_chart ();
-      //bubble_chart ();
-      //demo_table ();
+  d3.json("samples.json").then(function (data) {
+    var id_names = data.names;
+    id_names.forEach((id) => {
+      dropdownMenu.append("option").text(id).property("value", id);
     });
+    demo_table(id_names[0]);
+    chart(id_names[0])
+  });
 }
-dropdown()
+dropdown();
 
 //Create the Demographic Table
-function demo_table (filter_id) {
-  var tablemenu = d3.select("#sample-metadata");
-  d3.json("samples.json").then(function(data) {
-      var metadatapull = data.metadata;
-      var filter_data = metadatapull.filter(elem => elem.id==filter_id)
-      id_names.forEach((id) => {
-        dropdownMenu.append("option").text(id).property("value",id)
-
-
-        
+function demo_table(filter_id) {
+  var table_menu = d3.select("#sample-metadata");
+  table_menu.html("");
+  d3.json("samples.json").then(function (data) {
+    var metadata_pull = data.metadata;
+    var filter_data = metadata_pull.filter((elem) => elem.id == filter_id);
+    console.log(filter_data);
+    Object.entries(filter_data[0]).forEach(([key, value]) => {
+      table_menu.append("h6").text(`${key}: ${value}`);
     });
-
-//{"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0},
-
-      metadatapull.filter((id) => {
-          tablemenu.append("option").text(id).property("value",id)
-      });
-      //bar_chart ();
-      //bubble_chart ();
-      //demo_table ();
-    });
+  });
 }
 
+//Create chart function for both the bar and bubble graph
+//Filter data like the demo_table data.samples . Filtering for sample id  to match the filter id
+function chart(filter_id) {
+  d3.json("samples.json").then(function (data) {
+    var samples_pull = data.samples;
+    var samples_data = samples_pull.filter((elem) => elem.id == filter_id);
+    var metadata_pull = data.metadata;
+    var filter_data = metadata_pull.filter((elem) => elem.id == filter_id);
 
+    //Extract data for the x & y values of the bar chart
+    var result = samples_data[0];
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
+    //Get the top 10 otu_ids = y values
+    var top_10 = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
-//Create bar chart
+    var data = [
+      {
+        type: "bar",
+        y: top_10,
+        x: sample_values.slice(0, 10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        orientation: "h",
+      },
+    ];
 
+    //Create layout for bar chart
+    var barLayout = {
+      title: "Top 10 OTUs Found",
+      margin: { t: 30, l: 150 }
+    };
+      
 
-// var data = [{
-//   type: 'bar',
-//   x: [20, 14, 23],
-//   y: ['giraffes', 'orangutans', 'monkeys'],
-//   orientation: 'h'
-// }];
+    // Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bar", data, barLayout);
 
-// Plotly.newPlot('myDiv', data);
+    //Create bubble chart
+    //Extract data for the x & y values of the bar chart
+    var result = samples_data[0];
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
+    //Create trace for bubble chart
+    var trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      mode: "markers",
+      marker: {
+        size: [40, 60, 80, 100],
+      },
+    };
 
+    var data = [trace1];
 
+    var layout = {
+      title: "Marker Size",
+      showlegend: false,
+      height: 600,
+      width: 600,
+    };
 
-
-
-
-
-
-
-
-
-
-// let books = ["The Visual Display of Quantitative Information", "Automate the Boring Stuff", "Data Science from Scratch"]
-
-// let timesRead = [100, 50, 25]
-
-// let trace1 = {
-//   x: timesReads,
-//   y: books,
-//   type: 'bar',
-//   orientation: 'h'
-// };
-
-// let data = [trace1];
-
-// let layout = {
-//   title: title
-// };
-
-// Plotly.newPlot("plot", data, layout);
+    Plotly.newPlot("bubble", data, layout);
+  });
+}
